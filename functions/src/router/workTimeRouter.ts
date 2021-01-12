c58d412d2efdc966161585e6b9941d3ea4d6c9fd
junglesub <42944002+junglesub@users.noreply.github.com>
2021-01-13 01:13:02 +0900

    Function: Start and Stop Endpoint

--- functions/src/router/workTimeRouter.ts
+++ functions/src/router/workTimeRouter.ts
@@ -4,4 +4,54 @@ import * as admin from "firebase-admin";
 export const workTimeRouter = express.Router();
 
 // /start - Start Working
-workTimeRouter.post("/start", (req, res) => {});
+workTimeRouter.get("/start", async (req, res) => {
+  const data = (
+    await admin
+      .firestore()
+      .collection("user_worktime")
+      .doc(req.decodedToken.uid)
+      .get()
+  ).data();
+  if (!data || !data.since) {
+    admin
+      .firestore()
+      .collection("user_worktime")
+      .doc(req.decodedToken.uid)
+      .update({
+        since: new Date(),
+        // since: admin.firestore.Timestamp.now(),
+      });
+    return res.sendStatus(200);
+  }
+  return res.sendStatus(422);
+});
+
+// /stop - Stop/Finish Working
+workTimeRouter.get("/stop", async (req, res) => {
+  const data = (
+    await admin
+      .firestore()
+      .collection("user_worktime")
+      .doc(req.decodedToken.uid)
+      .get()
+  ).data();
+  if (data?.since) {
+    console.log("Date Since", data.since);
+    const currDur = Math.round(
+      (new Date().getTime() - data.since.toDate().getTime()) / 1000
+    );
+    console.log("currDur == ", currDur);
+    admin
+      .firestore()
+      .collection("user_worktime")
+      .doc(req.decodedToken.uid)
+      .update({
+        since: null,
+        duration: !isNaN(data.duration) ? data.duration + currDur : currDur,
+      });
+    return res.json({
+      currDur,
+    });
+  }
+  return res.sendStatus(422);
+});
